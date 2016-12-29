a712d6f9699e2f34f369423e7e0b21afc58a55e5
Yung Yuan <yungyuan@xditede-iMac.local>
2016-12-29 20:23:13 +0800

    add posts nested route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :groups
+  resources :groups do
+    resources :posts
+  end
 end
